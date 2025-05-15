538bca63abf817a7dff251add0c17512a9ab8e04
LeoSole423 <leonel.sole423@gmail.com>
2025-05-15 15:50:17 -0300

    liempeiza de codigo

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,8 @@
 #include "../include/quad.h"
 #include "../include/sma_filter.h"
 
-// Definir el tamaño máximo de la ventana para el SMA
-#define MAX_WINDOW_SIZE 50
-#define NUM_SIGNALS 6            // Número de señales paralelas
+
 #define STEERING_THRESHOLD 15000 // Threshold para bloquear el volante
-
-// Estructura para almacenar los datos de cada señal
-struct SMAData {
-  int values[MAX_WINDOW_SIZE];
-  int index;
-  int sum;
-  int count;
-};
-
-// Crear un array de estructuras para las 6 señales
-SMAData signals[NUM_SIGNALS];
 
 enum Estado {
   READY,
@@ -59,32 +46,15 @@ Servo myServo;
 int new_MaxVel = 1;
 bool maxvel_funcition_aux = true;
 
-// Crear objeto iBus
-IBusBM ibus;
-
 // Variables de canal
 int ch1Value, ch2Value, ch3Value, ch4Value, ch5Value, ch6Value;
 int sma_ch1, sma_ch2, sma_ch3, sma_ch4, sma_ch5, sma_ch6;
 int anglePot, angle;
 
-// Variables para control por UART3
-// Los comandos recibidos deberán tener:
-// Giro: -100 a 100, Acelerador: 0 a 255 y Frenos: 0 (falso) o 1 (verdadero)
-int uartSteering = 0;
-int uartThrottle = 0;
-bool uartBrake = false;
-
-// Flag para indicar que se ha solicitado telemetría
-bool telemetryRequested = false;
-
 // Declaración de funciones
-void initSMA();
-int SMAFilter(int signalIndex, int newValue, int windowSize);
 void checkSteeringStatus();
 void SteeringBloqLogic();
 void printInfo();
-void parseUARTCommand(String cmd);
-void sendTelemetry(int steering, int throttle, bool brake);
 
 //
 // ******************* setup() *******************
@@ -92,14 +62,12 @@ void setup() {
   Serial.begin(115200);
   Serial3.begin(115200); // Inicializa la UART3
 
-  initSMA();
-
+  
   // Configuración del PID
   PID1.SetMode(AUTOMATIC);
   PID1.SetOutputLimits(-255, 255);
   PID1.SetSampleTime(30);
 
-  initFS_IA6(CH1, CH2, CH3, CH4, CH5, CH6);
   pinMode(AnglePot, INPUT); // Potenciómetro
 
   initQuad();
@@ -109,101 +77,24 @@ void setup() {
 //
 // ******************* loop() *******************
 void loop() {
-  // Leer valores de los canales del control remoto
-  ch1Value = readChannel(CH1, -100, 100, 0);
-  ch2Value = readChannel(CH2, -100, 100, 0);
-  ch3Value = readChannel(CH3, -100, 100, -100);
-  ch4Value = readChannel(CH4, -100, 100, 0);
-  ch5Value = readChannel(CH5, 0, 100, 0);
-  ch6Value = readChannel(CH6, 0, 100, 0);
 
-  // Aplicar filtro SMA a cada canal
-  sma_ch1 = SMAFilter(0, ch1Value, 3);
-  sma_ch2 = SMAFilter(1, ch2Value, 3);
-  sma_ch3 = SMAFilter(2, ch3Value, 5);
-  sma_ch4 = SMAFilter(3, ch4Value, 5);
-  sma_ch5 = SMAFilter(4, ch5Value, 15);
-  sma_ch6 = SMAFilter(5, ch6Value, 8);
 
-  // Determinar el modo de control vía UART: activado si ch6 (filtrado) > 66
-  bool modoUART = (sma_ch6 > 66);
+  generatePWM(sma_ch3);
+  change_MaxVel(sma_ch2, new_MaxVel, maxvel_funcition_aux);
+  Quad_Reverse(sma_ch6);
+  Front_Brakes(sma_ch2);
+  Back_Brakes(sma_ch2);
 
-  // Procesar comandos recibidos por UART3 (para control o telemetría)
-  if (Serial3.available() > 0) {
-    String cmd = Serial3.readStringUntil('\n');
-    cmd.trim();
-    if (cmd.equals("GET_TELEMETRY")) {
-      telemetryRequested = true;
-    } else {
-      // Si no es el comando de telemetría, se asume que es de control
-      if (modoUART) {
-        parseUARTCommand(cmd);
-      }
-    }
-  }
+  // Control PID para el giro
+  anglePot = analogRead(AnglePot);
+  Setpoint1 = map(sma_ch1, -100, 100, 30, 170);
+  Input1 = map(anglePot, 0, 1023, 0, 270);
+  PID1.Compute();
 
-  // Determinar los valores activos de control según el modo
-  // controlGiro: de -100 a 100
-  // controlAcelerador: de 0 a 255
-  // controlFrenos: verdadero (aplicados) o falso
-  int controlGiro;
-  int controlAcelerador;
-  bool controlFrenos;
+  // Chequear estado y aplicar lógica de giro
+  checkSteeringStatus();
+  SteeringBloqLogic();
   
-  if (modoUART) {
-    // Se utilizan los comandos recibidos por UART3
-    controlGiro = uartSteering;
-    controlAcelerador = uartThrottle;
-    controlFrenos = uartBrake;
-  } else {
-    // Se utilizan las señales del control remoto
-    controlGiro = sma_ch1;
-    controlAcelerador = sma_ch3;
-    controlFrenos = (sma_ch2 < -10);
-  }
-
-  // Solo se ejecuta el control si el vehículo está “encendido”
-  if (Quad_ON(sma_ch5)) {
-    // Control del acelerador y frenos
-    if (modoUART) {
-      generatePWM(controlAcelerador);
-      if (controlFrenos) {
-        Front_Brakes(-60);
-        Back_Brakes(-60);
-      } else {
-        Front_Brakes(0);
-        Back_Brakes(0);
-      }
-      // En modo UART se omiten funciones propias del remoto (change_MaxVel, Quad_Reverse, etc.)
-    } else {
-      generatePWM(sma_ch3);
-      change_MaxVel(sma_ch2, new_MaxVel, maxvel_funcition_aux);
-      Quad_Reverse(sma_ch6);
-      Front_Brakes(sma_ch2);
-      Back_Brakes(sma_ch2);
-    }
-
-    // Control PID para el giro
-    anglePot = analogRead(AnglePot);
-    angle = map(anglePot, 0, 1023, 0, 270);
-    if (modoUART) {
-      Setpoint1 = map(controlGiro, -100, 100, 30, 170);
-    } else {
-      Setpoint1 = map(sma_ch1, -100, 100, 30, 170);
-    }
-    Input1 = map(anglePot, 0, 1023, 0, 270);
-    PID1.Compute();
-
-    // Chequear estado y aplicar lógica de giro
-    checkSteeringStatus();
-    SteeringBloqLogic();
-  }
-
-  // Enviar telemetría por UART3 solo si se ha recibido el comando de solicitud
-  if (telemetryRequested) {
-    sendTelemetry(controlGiro, controlAcelerador, controlFrenos);
-    telemetryRequested = false;
-  }
 
   // Imprimir información para debug en Serial
   printInfo();
@@ -213,56 +104,6 @@ void loop() {
 
 //
 // ******************* Funciones Auxiliares *******************
-
-// Inicializar filtro SMA
-void initSMA() {
-  for (int i = 0; i < NUM_SIGNALS; i++) {
-    signals[i].index = 0;
-    signals[i].sum = 0;
-    signals[i].count = 0;
-    for (int j = 0; j < MAX_WINDOW_SIZE; j++) {
-      signals[i].values[j] = 0;
-    }
-  }
-}
-
-// Filtrar señal con SMA
-int SMAFilter(int signalIndex, int newValue, int windowSize) {
-  if (windowSize > MAX_WINDOW_SIZE) {
-    windowSize = MAX_WINDOW_SIZE;
-  }
-  signals[signalIndex].sum -= signals[signalIndex].values[signals[signalIndex].index];
-  signals[signalIndex].values[signals[signalIndex].index] = newValue;
-  signals[signalIndex].sum += newValue;
-  signals[signalIndex].index = (signals[signalIndex].index + 1) % windowSize;
-  if (signals[signalIndex].count < windowSize) {
-    signals[signalIndex].count++;
-  }
-  return signals[signalIndex].sum / signals[signalIndex].count;
-}
-
-// Función para parsear comandos de control recibidos por UART3
-// Se espera un formato: "giro,acelerador,freno" (por ejemplo: "50,200,1")
-void parseUARTCommand(String cmd) {
-  int pos1 = cmd.indexOf(',');
-  int pos2 = cmd.indexOf(',', pos1 + 1);
-  if (pos1 == -1 || pos2 == -1) return;
-  
-  String strGiro = cmd.substring(0, pos1);
-  String strAcel = cmd.substring(pos1 + 1, pos2);
-  String strFreno = cmd.substring(pos2 + 1);
-  
-  uartSteering = strGiro.toInt();
-  uartThrottle = strAcel.toInt();
-  uartBrake = (strFreno.toInt() != 0);
-}
-
-// Función para enviar telemetría por UART3
-// Se envía una línea con el siguiente formato: giro,acelerador,freno
-void sendTelemetry(int steering, int throttle, bool brake) {
-  String telemetry = String(steering) + "_" + String(throttle) + "_" + (brake ? "1" : "0");
-  Serial3.println(telemetry);
-}
 
 // Chequear el estado del Steering (sin modificaciones)
 void checkSteeringStatus() {
